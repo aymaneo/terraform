08307de6469fb9a7a011ab7788f25f11bd88211f
yousoumar <oumaiyr@gmail.com>
2025-12-02 14:41:51 +0100

    Make outputs variables dynamic for Docker deployment

--- terraform/docker/outputs.tf
+++ terraform/docker/outputs.tf
@@ -1,10 +1,9 @@
-output "nginx_url" {
-  description = "URL to voting app"
-  value       = "http://localhost:8000"
+output "vote_nginx_loadbalancer_url" {
+  description = "URL to vote service nginx load balancer"
+  value       = "http://localhost:${docker_container.nginx.ports[0].external}"
 }
 
 output "result_url" {
   description = "URL to results page"
-  value       = "http://localhost:5050"
+  value       = "http://localhost:${docker_container.result.ports[0].external}"
 }
-
